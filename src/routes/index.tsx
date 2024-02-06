22b247eb77b5794416068ae3767e37a38dfe134b
sklbz <sokolbozanic@gmail.com>
2024-02-06 20:41:32 +0100

    ,

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,9 @@ export default component$(() => {
   return (
     <>
       <h1>Money Transfer. Faster. More secure.</h1>
+      <dialog id="cookies">
+        Si vous acceptez les cookies, nous les utiliserons pour améliorer votre expérience et permettre à nos partenaires de vous présenter des publicités personnalisées lorsque vous visitez d'autres sites.
+      </dialog>
     </>
   );
 });
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -20,7 +20,7 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
-    date: "new Date().toISOString()",
+    date: new Date().toISOString(),
   };
 });
 
