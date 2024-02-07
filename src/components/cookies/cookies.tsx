69b3304a1128df44c8a1c06080dc3b41d92ebd7f
sklbz <sokolbozanic@gmail.com>
2024-02-07 22:13:00 +0100

    test local storage

--- src/components/cookies/cookies.tsx
+++ src/components/cookies/cookies.tsx
@@ -1,18 +1,23 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
+import Style from "./cookies.css?inline";
 
 export default component$(() => {
+
+  useStylesScoped$(Style);
 
   const modalVisible = useSignal(true);
 
   return (
     <>
       {modalVisible.value && (
-        <div id="cookies">
-          <p>
-          Si vous acceptez les cookies, nous les utiliserons pour améliorer votre expérience et permettre à nos partenaires de vous présenter des publicités personnalisées lorsque vous visitez d'autres sites.
-          </p>
-          <button onClick$={() => modalVisible.value = false}> Accepter </button>
-          <button onClick$={() => modalVisible.value = false}> Refuser </button>
+        <div class="cookie-container">
+          <div id="cookies" class="cookie">
+            <p>
+            Si vous acceptez les cookies, nous les utiliserons pour améliorer votre expérience et permettre à nos partenaires de vous présenter des publicités personnalisées lorsque vous visitez d'autres sites.
+            </p>
+            <button onClick$={() => modalVisible.value = false}> Accepter </button>
+            <button onClick$={() => modalVisible.value = false}> Refuser </button>
+          </div>
         </div>
       )}
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import Style from "./index.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(Style);
+  
   return (
     <>
       <h1>Money Transfer. Faster. More secure.</h1>
-      
+      <section></section>
+      <button onClick$={() => {
+          localStorage.setItem("theme", "hello")
+        }}>click</button>
     </>
   );
 });
